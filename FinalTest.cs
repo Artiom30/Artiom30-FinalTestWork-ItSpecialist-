05cd0a3efd1cb32b3c7eaaf73f6d028a6e09800d
Artiom30 <mezheynikov1@gmail.com>
2022-11-28 17:35:51 +0300

    Task Version 1

--- FinalTest.cs
+++ FinalTest.cs
@@ -1,1 +1,31 @@
-﻿
+﻿string[] array = { "Happy", "new", "year", "and", "merry", "christmas." };
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"[{array[i]}] ");
+
+    Console.WriteLine();
+}
+
+ShowArray(array);
+
+string[] CutStringArray(string[] array)
+{
+    int length = 0;
+    string[] cutArray = new string[array.Length];
+    for (int i = 0, j = 0; i < array.Length; i++, j++)
+    {
+        length = new Random().Next(0, 4);
+        while (array[i].Length > length)
+        {
+            array[i] = array[i].Remove(0, 1);
+        }
+        cutArray[j] = array[i];
+    }
+    return cutArray;
+}
+
+Console.WriteLine();
+string[] cutArray = CutStringArray(array);
+ShowArray(cutArray);
